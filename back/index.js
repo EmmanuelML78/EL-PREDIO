5212b2b0f84e6c5a1fee3a18a21c0f9a48aaa2a0
franco mansilla <mansilla.franco.1@gmail.com>
2023-04-12 12:57:20 -0300

    problema db solucionado

--- back/index.js
+++ back/index.js
@@ -1,6 +1,10 @@
 const app = require("./src/app");
 const { conn } = require("./src/db.js");
 
+// app.listen(3001, () => {
+//   console.log("listening on port 3001");
+// });
+
 conn.sync({ force: true }).then(() => {
   app.listen(3001, () => {
     console.log("%s listening at 3001");
