b7925d21b21f0a3bb32be8786807721ec36811cf
KailyKinG <kailyking@gmail.com>
2023-04-12 19:26:33 -0400

    Se Agregaron los archivos .jsx y .module.css de: Card, Profile y DashBoard

---
+++ front/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import c from "./Card.module.css";
+// import styled from "styled-components";
+
+const Card = (props) => {
+	return (
+		<>
+			<h1>Este Es El Componente Card</h1>
+		</>
+	);
+};
+
+export default Card;
---
+++ front/src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import d from "./DashBoard.module.css";
+// import styled from "styled-components";
+
+const DashBoard = (props) => {
+	return (
+		<>
+			<h1>Este Es El Componente DashBoard</h1>
+		</>
+	);
+};
+
+export default DashBoard;
