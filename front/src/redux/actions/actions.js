69bcb5fe1e81d14f29eec62f7a5ede0bc4a32c23
KailyKinG <kailyking@gmail.com>
2023-04-12 15:02:15 -0400

    Se Agregaron las unidades de redux, se configuro react-router-dom, la configuracion de coneccion en el archivo main.jsx lista

---
+++ front/src/redux/actions/actions.js
@@ -0,0 +1,1 @@
+// Aqui las actions creator
---
+++ front/src/redux/reducer/elprediofutbolReducer.js
@@ -0,0 +1,19 @@
+// Importar Primero Los Tipos De acciones
+
+// Estado Inicial
+const initialState = {
+	canchas: [],
+	allCanchas: [],
+	detailCancha: [],
+};
+
+const elprediofutbolReducer = (state = initialState, action) => {
+	switch (action.type) {
+		// A Completar
+
+		default:
+			return state;
+	}
+};
+
+export default elprediofutbolReducer;
