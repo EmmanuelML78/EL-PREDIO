18ec56aded826513d8c1ea6987d78ea9d979ee41
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-04-14 08:43:46 -0500

    rutas de get, post y put terminadas

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,5 +1,13 @@
 const { Router } = require("express");
-
 const router = Router();
+const morgan = require("morgan");
+const express = require("express");
+const reservaRoute = require("./reservaRoute")
+
+
+router.use(express.json());
+router.use(morgan("dev"));
+router.use("/reserva", reservaRoute)
+
 
 module.exports = router;
---
+++ back/src/routes/reservaRoute.js
@@ -0,0 +1,59 @@
+const router = require('express').Router()
+const { getAllReservations, deleteReserva } = require('../controllers/ReservaControllers')
+const { Reserva } = require('../db')
+
+router.get("/", async (req, res) => {
+    let allreserva = await getAllReservations();
+    try {
+      res.status(200).send(allreserva);
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    let allreserva = await getAllReservations();
+    try {
+      if (id) {
+        const reservaid = await allreserva.filter((el) => el.id == id);
+        id.length
+        ? res.status(200).send(reservaid)
+        : res.status(500).json({ message: "Error al obtener Reserva por ID" });
+      }
+    } catch (error) {
+      throw error
+    }
+  });
+
+
+router.post('/', async (req, res) => {
+    const { date, start, end, status, hasPromo } = req.body;
+    try {
+      const reservation = await Reserva.create({
+        date,
+        start,
+        end,
+        status,
+        hasPromo,
+      });
+      res.status(201).json(reservation);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear la reserva' });
+    }
+  });
+
+  router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      const deletedReserva = await deleteReserva(id);
+      deletedReserva
+        ? res.status(200).json({ message: "Reserva eliminada con éxito" })
+        : res.status(404).json({ message: "Reserva no encontrada" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al eliminar la Reserva" });
+    }
+  });
+
+  module.exports = router;
