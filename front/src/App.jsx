f5b0d1084f768ad0be1ac3d0f9160f1f2570b18f
Frann131 <franmindurry@gmail.com>
2023-04-14 15:39:20 -0300

    arregle importacion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Landing from "./components/Landing";
+import Landing from "./components//Landing/Landing.jsx";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
