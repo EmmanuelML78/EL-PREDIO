60182893c668c25686a9fbe64747f896ed220d49
Frann131 <franmindurry@gmail.com>
2023-04-14 22:04:35 -0300

    actions modularizadas, falta logica

---
+++ front/src/redux/actions/reservaActions.js
@@ -0,0 +1,12 @@
+export const GET_RESERVAS_BY_USER = "GET_RESERVAS_BY_USER";
+export const GET_RESERVAS_BY_CANCHA = "GET_RESERVAS_BY_CANCHA";
+export const POST_RESERVA = "POST_RESERVA";
+export const DELETE_RESERVA = "DELETE_RESERVA";
+
+export const getReservasByUser = (userId) => {};
+
+export const getReservasByCancha = (canchaId) => {};
+
+export const postReserva = (reservaData) => {};
+
+export const deleteReserva = (reservaId) => {};
