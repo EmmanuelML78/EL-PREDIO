659520dc3610fa996008911ea03a23ef78d2fd7b
Frann131 <franmindurry@gmail.com>
2023-04-16 20:40:42 -0300

    detail hc

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,18 +2,22 @@ import "./App.css";
 import Landing from "./components//Landing/Landing.jsx";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Detail from "./components/Detail/Detail";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <>
       <Switch>
-		<Route exact path="/">
-			<Landing />
-		</Route>
+        <Route exact path="/">
+          <Landing />
+        </Route>
         <Route path="/home">
           <Home />
           <Footer />
+        </Route>
+        <Route path="/canchas/detail">
+          <Detail/>
         </Route>
       </Switch>
     </>
