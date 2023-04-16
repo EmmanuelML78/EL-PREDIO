a6348bd6489d03a344abc5e4d44a151637d0eccd
franco mansilla <mansilla.franco.1@gmail.com>
2023-04-16 19:04:12 -0300

    creeDashBoard

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,19 +2,21 @@ import "./App.css";
 import Landing from "./components//Landing/Landing.jsx";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import DashBoard from "./components/DashBoard/DashBoard";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <>
       <Switch>
-		<Route exact path="/">
-			<Landing />
-		</Route>
+        <Route exact path="/">
+          <Landing />
+        </Route>
         <Route path="/home">
           <Home />
           <Footer />
         </Route>
+        <Route exact path="/dashboard" component={DashBoard} />
       </Switch>
     </>
   );
