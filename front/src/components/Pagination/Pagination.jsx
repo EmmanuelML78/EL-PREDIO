b15b6b6984c9ccdfc07ad0160af284b9e44b63f1
spawn22 <todococo14@gmail.com>
2023-04-16 10:34:06 -0300

    Paginado

--- front/src/components/Pagination/Pagination.jsx
+++ front/src/components/Pagination/Pagination.jsx
@@ -3,12 +3,30 @@ import React from "react";
 // import p from "./Pagination.module.css";
 // import styled from "styled-components";
 
-const Pagination = (props) => {
+const Pagination = ({ itemsPerPage, totalItems, currentPage, setCurrentPage }) => {
+	const handleClickNext = () => {
+	  if (currentPage < Math.ceil(totalItems / itemsPerPage)) {
+		setCurrentPage(currentPage + 1);
+	  }
+	};
+  
+	const handleClickPrev = () => {
+	  if (currentPage > 1) {
+		setCurrentPage(currentPage - 1);
+	  }
+	};
+  
 	return (
-		<>
-			<h1>Este Es El Componente Pagination</h1>
-		</>
+	  <div style={{ display: 'flex', justifyContent: 'center', marginBottom: "1.5rem" }}>
+		<button onClick={handleClickPrev} disabled={currentPage === 1}>
+		  {'<'}
+		</button>
+		<span style={{marginLeft: "1rem", marginRight: "1rem", fontSize: "1.5rem"}}>{currentPage}</span>
+		<button onClick={handleClickNext} disabled={currentPage === Math.ceil(totalItems / itemsPerPage)}>
+		  {'>'}
+		</button>
+	  </div>
 	);
-};
+  };
 
 export default Pagination;
