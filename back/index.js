ef4c3325a6dd6947d97ab48e89a7d26e8829e39f
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-04-19 19:12:16 -0500

    cambios para el deploy

--- back/index.js
+++ back/index.js
@@ -1,12 +1,14 @@
 const app = require("./src/app");
 const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { PORT } = process.env;
 
 // app.listen(3001, () => {
 //   console.log("listening on port 3001");
 // });
 
 conn.sync({ alter: true }).then(() => {
-  app.listen(3001, () => {
-    console.log("%s listening at 3001");
+  app.listen(PORT, () => {
+    console.log("%s listening at", PORT);
   });
 });
