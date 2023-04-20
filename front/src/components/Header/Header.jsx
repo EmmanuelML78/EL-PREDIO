accd81721c5620db0d0d129a59a0219b0e45cab9
spawn22 <todococo14@gmail.com>
2023-04-20 11:08:13 -0300

    asdasd

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,28 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
-import icon from "../../assets/icon.png";
-import "./Header.css";
 import styles from './Header.module.css';
 
 function Header() {
-  return (
-    <header>
-      <div className="header-container">
-        <Link to="/">
-          <img
-            src={icon}
-            alt="cancha"
-            style={{ height: "5rem", width: "5rem" }}
-          />
-        </Link>
-        <Link to="/home">
-          <h1 className="nav-title">El Predio</h1>
-        </Link>
-        <div className="user">
-          <Link style={{ color: "white" }} to="/dashboard">
-            <FaUser />
-          </Link>
     return (
     <header className={styles.header}>
         <div className={styles.contenedor}>
@@ -57,31 +35,6 @@ function Header() {
             <label className={styles.label} for="fecha">Fecha</label>
             <input type="date" className={styles.input} id="fecha"/>
         </div>
-      </div>
-      <nav className="navbar">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/reservas" className="nav-link">
-              Reservas
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contacto" className="nav-link">
-              Contacto
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/faq" className="nav-link">
-              Nosotros
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/faq" className="nav-link">
-              FAQ
-            </Link>
-          </li>
-        </ul>
-      </nav>
 
         <div className={styles.campo}>
             <label className={styles.label} for="hora">Horario</label>
@@ -94,7 +47,6 @@ function Header() {
 
         </form>
     </header>
-  );
     );
 }
 
