f1ad8088a7155f24b39308fb51833ed3dff16281
Frann131 <franmindurry@gmail.com>
2023-04-21 20:29:39 -0300

    base url configurada para peticiones

--- front/src/redux/actions/authActions.js
+++ front/src/redux/actions/authActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import instance from "../axiosCfg";
 
 export const LOGIN_USER = "LOGIN_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
@@ -7,7 +7,7 @@ export const SET_USER = "SET_USER";
 
 export const loginUser = (userData) => {
     return async function (dispatch) {
-      const response = await axios.post("http://localhost:3001/login", userData);
+      const response = await instance.post("login", userData);
       const token = response.data.token;
       localStorage.setItem("token", token);
       dispatch({
