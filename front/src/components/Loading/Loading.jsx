2ec6577200baeac45b0d7de44d10c10d03d835f6
Frann131 <franmindurry@gmail.com>
2023-04-23 18:32:56 -0300

    mejora de manejo de autenticacion de usuario

--- front/src/components/Loading/Loading.jsx
+++ front/src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { collapseToast } from "react-toastify";
+import spinner from "../../assets/Spinner-1.1s-200px.gif"
 
 // import l from "./Loading.module.css";
 // import styled from "styled-components";
@@ -8,7 +8,7 @@ const Loading = (props) => {
 	return (
 		<div style={{display: "flex", flexDirection: "column"}}>
 			<h2 style={{color: "white"}}>Cargando...</h2>
-			<img src="" alt="" />
+			<img src={spinner} alt="" />
 		</div>
 	);
 };
