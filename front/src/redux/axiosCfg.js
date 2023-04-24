62845f37c299d3248c53fde3d24febd11cae5a8c
Katsuragui <51885387+Seijuro6@users.noreply.github.com>
2023-04-24 11:08:22 -0500

    cambio ruta axios

--- front/src/redux/axiosCfg.js
+++ front/src/redux/axiosCfg.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "el-predio-production-32b7.up.railway.app/",
 });
 
 instance.interceptors.request.use((config) => {
