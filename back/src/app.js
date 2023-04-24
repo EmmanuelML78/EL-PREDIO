98c901db007914d52aae98cdc820fb99bd35e85c
Harold Cadena <astarot6.6@hotmail.com>
2023-04-24 11:50:06 -0500

    "cambio ruta nuevo vercel"

--- back/src/app.js
+++ back/src/app.js
@@ -23,7 +23,7 @@ app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://el-predio-sigma.vercel.app"
+    "https://el-predio-fu4q.vercel.app"
   );
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
