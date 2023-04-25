3fe4932b22685faeacb8e4c5737588d46ac4b38c
spawn22 <todococo14@gmail.com>
2023-04-25 15:40:56 -0300

    Perfil user

--- front/src/components/Profile/Profile.jsx
+++ front/src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "../../redux/actions/authActions";
+import { setUser, editUser } from "../../redux/actions/authActions";
 import Loading from "../Loading/Loading.jsx";
 import DashBoard from "../DashBoard/DashBoard";
 import "./Profile.css";
@@ -10,7 +10,20 @@ import moment from "moment";
 const Profile = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
+  console.log(user);
   const [isLoading, setIsLoading] = useState(true);
+  const [editName, setEditName] = useState(user.name);
+  const [editLastName, setEditLastName] = useState(user.lastName);
+  const [editEmail, setEditEmail] = useState(user.email);
+  const [editPassword, setEditPassword] = useState(user.password);
+  const [editPhone, setEditPhone] = useState(user.phone);
+
+  const [showNameInput, setShowNameInput] = useState(false);
+  const [showLastNameInput, setShowLastNameInput] = useState(false);
+  const [showEmailInput, setShowEmailInput] = useState(false);
+  const [showPasswordInput, setShowPasswordInput] = useState(false);
+  const [showPhoneInput, setShowPhoneInput] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       if (!user) {
@@ -20,6 +33,27 @@ const Profile = () => {
     };
     fetchData();
   }, [dispatch, user]);
+
+  const handleNameChangeClick = async () => {
+    await dispatch(editUser({ name: editName }));
+  };
+
+  const handleLastNameChangeClick = async () => {
+    await dispatch(editUser({ lastName: editLastName }));
+  };
+
+  const handleEmailChangeClick = async () => {
+    await dispatch(editUser({ email: editEmail }));
+  };
+
+  const handlePasswordChangeClick = async () => {
+    await dispatch(editUser({ password: editPassword }));
+  };
+
+  const handlePhoneChangeClick = async () => {
+    await dispatch(editUser({ phone: editPhone }));
+  };
+
   return (
     <>
       {isLoading ? (
@@ -27,56 +61,219 @@ const Profile = () => {
       ) : user.isAdmin ? (
         <DashBoard />
       ) : (
-        <>
-          <h1 style={{ color: "white", fontWeight: "600" }}>Panel de perfil</h1>
-          <div className="profile-container">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                placeItems: "flex-start",
-                width: "40rem",
-              }}
-            >
-              <div style={{ display: "flex", alignItems: "center", marginBottom:"1rem" }}>
-                <p style={{ marginRight: "1rem" }}>Nombre:</p>
-                <p>{user.name}</p>
+        <div style={{ marginTop: "25rem", marginBottom: "20rem" }}>
+          <h1
+            style={{ color: "white", fontWeight: "600", marginBottom: "10rem" }}
+          >
+            Panel de perfil
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div className="profile-container">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ marginRight: "1rem", fontWeight: "900" }}>
+                  Nombre:
+                </p>
+                {showNameInput ? (
+                  <>
+                    <input
+                      style={{ width: "20rem" }}
+                      value={editName}
+                      onChange={(e) => setEditName(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <span>{user.name}</span>
+                )}
+                <button
+                  onClick={() => {
+                    if (showNameInput) {
+                      handleNameChangeClick();
+                    }
+                    setShowNameInput(!showNameInput);
+                  }}
+                  style={{
+                    height: "4rem",
+                    marginLeft: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cambiar
+                </button>
               </div>
-              <div style={{ display: "flex", alignItems: "center", marginBottom:"1rem" }}>
-                <p style={{ marginRight: "1rem" }}>Apellido:</p>
-                <p>{user.lastName}</p>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ marginRight: "1rem", fontWeight: "900" }}>
+                  Apellido:
+                </p>
+                {showLastNameInput ? (
+                  <>
+                    <input
+                      style={{ width: "20rem" }}
+                      value={editLastName}
+                      onChange={(e) => setEditLastName(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <span>{user.lastName}</span>
+                )}
+                <button
+                  onClick={() => {
+                    if (showLastNameInput) {
+                      handleLastNameChangeClick();
+                    }
+                    setShowLastNameInput(!showLastNameInput);
+                  }}
+                  style={{
+                    height: "4rem",
+                    marginLeft: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cambiar
+                </button>
               </div>
-              <div style={{ display: "flex", alignItems: "center", marginBottom:"1rem" }}>
-                <p style={{ marginRight: "1rem" }}>Email:</p>
-                <p>{user.email}</p>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ marginRight: "1rem", fontWeight: "900" }}>Email:</p>
+                {showEmailInput ? (
+                  <>
+                    <input
+                      style={{ width: "20rem" }}
+                      value={editEmail}
+                      onChange={(e) => setEditEmail(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <span>{user.email}</span>
+                )}
+                <button
+                  onClick={() => {
+                    if (showEmailInput) {
+                      handleEmailChangeClick();
+                    }
+                    setShowEmailInput(!showEmailInput);
+                  }}
+                  style={{
+                    height: "4rem",
+                    marginLeft: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cambiar
+                </button>
               </div>
-              <div style={{ display: "flex", alignItems: "center", marginBottom:"1rem" }}>
-                <p style={{ marginRight: "1rem" }}>Miembro desde:</p>
-                <p>{moment(user.createdAt).format("DD/MM/YYYY")}</p>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ marginRight: "1rem", fontWeight: "900" }}>
+                  Contraseña:
+                </p>
+                {showPasswordInput ? (
+                  <>
+                    <input
+                      style={{ width: "20rem" }}
+                      onChange={(e) => setEditPassword(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <span>{"******"}</span>
+                )}
+                <button
+                  onClick={() => {
+                    if (showPasswordInput) {
+                      handlePasswordChangeClick();
+                    }
+                    setShowPasswordInput(!showPasswordInput);
+                  }}
+                  style={{
+                    height: "4rem",
+                    marginLeft: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cambiar
+                </button>
               </div>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                placeItems: "flex-start",
-              }}
-            >
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <button style={{ height: "4rem", marginBottom:"1rem" }}>Cambiar</button>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ marginRight: "1rem", fontWeight: "900" }}>
+                  Telefono:
+                </p>
+                {showPhoneInput ? (
+                  <>
+                    <input
+                      style={{ width: "20rem" }}
+                      value={editPhone}
+                      onChange={(e) => setEditPhone(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <span>{user.phone}</span>
+                )}
+                <button
+                  onClick={() => {
+                    if (showPhoneInput) {
+                      handlePhoneChangeClick();
+                    }
+                    setShowPhoneInput(!showPhoneInput);
+                  }}
+                  style={{
+                    height: "4rem",
+                    marginLeft: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cambiar
+                </button>
               </div>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <button style={{ height: "4rem", marginBottom:"1rem" }}>Cambiar</button>
-              </div>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <button style={{ height: "4rem", marginBottom:"1rem" }}>Cambiar</button>
-              </div>
-              <button style={{ marginTop: "1rem", height: "4rem" }}>
-                Cambiar contraseña
-              </button>
             </div>
           </div>
-        </>
+        </div>
       )}
     </>
   );
