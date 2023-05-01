49e9bc8c6b04d0ccf9150add7c47c02a2bc31fea
franco mansilla <mansilla.franco.1@gmail.com>
2023-05-01 10:28:26 -0300

    rutas creadas

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -4,11 +4,13 @@ const canchaRoute = require("./canchaRoutes");
 const reservaRoute = require("./reservaRoute");
 const reviewsRoute = require("./reviewRoute");
 const authRoute = require("./authRoute");
+const promoRoute = require("./promoRoute");
 
 const router = Router();
 
 router.use("/", authRoute);
 router.use("/", userRoute);
+router.use("/", promoRoute);
 router.use("/canchas", canchaRoute);
 router.use("/reserva", reservaRoute);
 router.use("/reviews", reviewsRoute);
