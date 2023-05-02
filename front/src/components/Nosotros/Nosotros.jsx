3c5c5e38db4ecd3de74d0c47ec33345aa13e865d
Harold Cadena <astarot6.6@hotmail.com>
2023-05-02 11:09:05 -0500

    "ubicacion"

--- front/src/components/Nosotros/Nosotros.jsx
+++ front/src/components/Nosotros/Nosotros.jsx
@@ -23,7 +23,7 @@ export const Nosotros = () => {
             </div>
             <iframe
               // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2083.2717413714836!2d-58.365951241429855!3d-34.635502965949534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a334b6925e5473%3A0x1ca5b2748858b40d!2sEstadio%20Alberto%20J.%20Armando!5e1!3m2!1ses!2sar!4v1682613507957!5m2!1ses!2sar"
-              src="https://www.google.com/maps/place/Canchas+El+Predio/@-34.5203742,-59.2772865,10z/data=!4m10!1m2!2m1!1scanchas+el+predio!3m6!1s0x95bcbdf126a79637:0xeed195e4523270d8!8m2!3d-34.5203742!4d-58.7005043!15sChFjYW5jaGFzIGVsIHByZWRpb-ABAA!16s%2Fg%2F11scpxytbr?hl=es-ES"
+              src="https://www.google.com/maps/place/Canchas+El+Predio/@-34.5202859,-58.7027618,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcbd25a08f5d15:0x9058655f36dbc4e9!8m2!3d-34.5202903!4d-58.7001869!16s%2Fg%2F11cs1wrllk"
               style={{
                 border: "0",
                 minWidth: "600px",
