430e018b68381fb6f5b33f40a0c43fcf6c144dbd
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-05-02 20:36:03 -0500

    borra

--- back/src/utils/mercadoPago.js
+++
@@ -1,9 +0,0 @@
-const mercadopago = require("mercadopago");
-require("dotenv").config();
-
-// Agrega credenciales
-mercadopago.configure({ access_token: process.env.ACCESS_TOKEN });
-
-module.exports = {
-  mercadopago,
-};
