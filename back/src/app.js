536322ae4af281d3aeaad81c46acd95e4928d937
Harold Cadena <astarot6.6@hotmail.com>
2023-05-03 13:02:35 -0500

    "app.js"

--- back/src/app.js
+++ back/src/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://el-predio.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
