d777acf4e980a7a7dc9f6451e16af59ceebe5c7b
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-05-09 13:10:52 -0500

    Update axiosCfg.js

--- front/src/redux/axiosCfg.js
+++ front/src/redux/axiosCfg.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://elpredio.onrender.com/",
+  baseURL: "https://el-predio-production.up.railway.app/",
 });
 
 instance.interceptors.request.use((config) => {
