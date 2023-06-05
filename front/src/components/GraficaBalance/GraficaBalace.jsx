a8e02b4a5c6db619e74953c9e872ab63af223f89
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-06-05 13:01:35 -0500

    grafica

--- front/src/components/GraficaBalance/GraficaBalace.jsx
+++ front/src/components/GraficaBalance/GraficaBalace.jsx
@@ -63,14 +63,38 @@ import {
   YAxis,
 } from "recharts";
 import { getBalance } from "../../redux/actions/balanceActions";
+import "./GraficaBalance.style.css";
 
 const GraficaBalace = () => {
-  const balance = useSelector((state) => state.balance);
+  const balance = useSelector((state) => state.balance.balance);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getBalance());
   }, [dispatch]);
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload; // Obtiene los datos del elemento seleccionado
+      const cierreCaja = data.cierreCaja; // Accede al valor de cierre de caja en los datos
+      const descripcion = data.descripcion; // Accede a la descripción en los datos
+
+      return (
+        <div className="custom-tooltip">
+          <div className="description">
+            <p className="description-label">Cierre de caja:</p>
+            <p className="description-text">{cierreCaja}</p>
+          </div>
+          <div className="description">
+            <p className="description-label">Descripción:</p>
+            <p className="description-text">{descripcion}</p>
+          </div>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div className="flex justify-center">
@@ -84,12 +108,11 @@ const GraficaBalace = () => {
             right: 30,
             left: 20,
             bottom: 5,
-          }}
-        >
+          }}>
           <CartesianGrid strokeDasharray="4 1 2" />
           <XAxis dataKey="createdAt" />
           <YAxis />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           <Legend />
           <Bar dataKey="cierreCaja" fill="#6b48ff" />
         </BarChart>
