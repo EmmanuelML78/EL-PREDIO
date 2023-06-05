c96b52cc047a01272a7f9dc99a150c615598a200
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-06-05 13:24:19 -0500

    grafica por mes

--- front/src/components/GraficaBalance/GraficaBalace.jsx
+++ front/src/components/GraficaBalance/GraficaBalace.jsx
@@ -50,6 +50,79 @@
 
 // export default GraficaBalace;
 
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+//   Bar,
+//   BarChart,
+//   CartesianGrid,
+//   Legend,
+//   ResponsiveContainer,
+//   Tooltip,
+//   XAxis,
+//   YAxis,
+// } from "recharts";
+// import { getBalance } from "../../redux/actions/balanceActions";
+// import "./GraficaBalance.style.css";
+
+// const GraficaBalace = () => {
+//   const balance = useSelector((state) => state.balance.balance);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(getBalance());
+//   }, [dispatch]);
+
+//   const CustomTooltip = ({ active, payload, label }) => {
+//     if (active && payload && payload.length) {
+//       const data = payload[0].payload; // Obtiene los datos del elemento seleccionado
+//       const cierreCaja = data.cierreCaja; // Accede al valor de cierre de caja en los datos
+//       const descripcion = data.descripcion; // Accede a la descripción en los datos
+
+//       return (
+//         <div className="custom-tooltip">
+//           <div className="description">
+//             <p className="description-label">Cierre de caja:</p>
+//             <p className="description-text">{cierreCaja}</p>
+//           </div>
+//           <div className="description">
+//             <p className="description-label">Descripción:</p>
+//             <p className="description-text">{descripcion}</p>
+//           </div>
+//         </div>
+//       );
+//     }
+
+//     return null;
+//   };
+
+//   return (
+//     <div className="flex justify-center">
+//       <ResponsiveContainer width="80%" aspect={2}>
+//         <BarChart
+//           data={balance}
+//           width={400}
+//           height={100}
+//           margin={{
+//             top: 5,
+//             right: 30,
+//             left: 20,
+//             bottom: 5,
+//           }}>
+//           <CartesianGrid strokeDasharray="4 1 2" />
+//           <XAxis dataKey="createdAt" />
+//           <YAxis />
+//           <Tooltip content={<CustomTooltip />} />
+//           <Legend />
+//           <Bar dataKey="cierreCaja" fill="#6b48ff" />
+//         </BarChart>
+//       </ResponsiveContainer>
+//     </div>
+//   );
+// };
+
+// export default GraficaBalace;
+
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -62,6 +135,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { format } from "date-fns"; // Importa la función format de date-fns
 import { getBalance } from "../../redux/actions/balanceActions";
 import "./GraficaBalance.style.css";
 
@@ -73,11 +147,27 @@ const GraficaBalace = () => {
     dispatch(getBalance());
   }, [dispatch]);
 
+  const dataByMonth = balance.reduce((acc, entry) => {
+    const month = format(new Date(entry.createdAt), "MMMM yyyy"); // Formato de mes y año
+    const existingData = acc.find((item) => item.month === month);
+
+    if (existingData) {
+      existingData.cierreCaja += entry.cierreCaja; // Suma el valor de cierre de caja al mes existente
+    } else {
+      acc.push({
+        month,
+        cierreCaja: entry.cierreCaja,
+      });
+    }
+
+    return acc;
+  }, []);
+
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
-      const data = payload[0].payload; // Obtiene los datos del elemento seleccionado
-      const cierreCaja = data.cierreCaja; // Accede al valor de cierre de caja en los datos
-      const descripcion = data.descripcion; // Accede a la descripción en los datos
+      const data = payload[0].payload;
+      const cierreCaja = data.cierreCaja;
+      const descripcion = data.descripcion;
 
       return (
         <div className="custom-tooltip">
@@ -100,7 +190,7 @@ const GraficaBalace = () => {
     <div className="flex justify-center">
       <ResponsiveContainer width="80%" aspect={2}>
         <BarChart
-          data={balance}
+          data={dataByMonth} // Utiliza dataByMonth en lugar de balance
           width={400}
           height={100}
           margin={{
@@ -110,7 +200,8 @@ const GraficaBalace = () => {
             bottom: 5,
           }}>
           <CartesianGrid strokeDasharray="4 1 2" />
-          <XAxis dataKey="createdAt" />
+          <XAxis dataKey="month" />{" "}
+          {/* Utiliza 'month' como la clave para el eje X */}
           <YAxis />
           <Tooltip content={<CustomTooltip />} />
           <Legend />
